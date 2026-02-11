621d76f7f0f35ffa6daed2ddee65a36abd8a83a7
Vintage <wildanmln874@gmail.com>
2026-02-11 19:08:11 +0700

    Create lib.rs

---
+++ lib.rs
@@ -0,0 +1,16 @@
+use anchor_lang::prelude::*;
+
+declare_id!("Vintage1111111111111111111111111111111111");
+
+#[program]
+pub mod vintage_token {
+    use super::*;
+
+    pub fn initialize(_ctx: Context<Initialize>) -> Result<()> {
+        msg!("Vintage initialized");
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize {}
